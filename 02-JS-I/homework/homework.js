58dd263195f23493bb5db10a4a610d1bf170bc36
unknown <mosqueralorenzo@hotmail+.com>
2022-06-02 09:00:46 -0300

    trabajos realizados hasta la linea 28

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = 'hola';
 const nuevoNum = 20;
 
 // Crea una variable booleana:
-const nuevoBool = true;
+const nuevoBool = false;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
@@ -16,7 +16,7 @@ const nuevaResta = 10 - 5 === 5;
 const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === 10.5;
+const nuevoModulo = 21 % 5 === 1;
 
 
 // En los próximos 22 problemas, deberás completar la función.
@@ -28,7 +28,6 @@ const nuevoModulo = 21 % 5 === 10.5;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  const str= "hola";
   return str;
 }
 
