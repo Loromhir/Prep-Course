3c261c94d0eadfa24e740cdf70375525c1824028
unknown <mosqueralorenzo@hotmail+.com>
2022-06-03 11:07:28 -0300

    tareas realizadas hasta la linea 142 del archivo homework de JSII

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -130,10 +130,10 @@ function fizzBuzz(numero) {
   // De lo contrario, devuelve el numero
   if (numero % 3 === 0){
     return "fizz"
-  }else if (numero % 5 === 0){
+  } if (numero % 5 === 0){
     return "buzz"
-  }else if (numero % 3 === 0 && numero % 5 === 0){
-    return "fizzbuzz"
+  }   if (numero % 3 === 0 && numero % 5 === 0){
+    return "fizzbuzz";
   }else{ 
     return numero;
   }
