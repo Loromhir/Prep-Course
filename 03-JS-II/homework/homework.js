3d6f379b2970a4854ecf10e531b176f97f89c3bd
unknown <mosqueralorenzo@hotmail+.com>
2022-06-04 09:13:41 -0300

    todas las tareas realizadas del homework JSII

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -204,7 +204,7 @@ function tablaDelSeis(){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  if (numero.length = 3){
+  if (numero.length = 2){
     return true;
   }else{
     return false;
