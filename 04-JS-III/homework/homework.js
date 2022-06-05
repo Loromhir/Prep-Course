bd876038e8294a90a87fc55a1843c4fe80c6b65d
unknown <mosqueralorenzo@hotmail+.com>
2022-06-05 18:39:42 -0300

    tarea realizada hasta la linea 133 del homework JSIII

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -125,6 +125,8 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  if(!arguments.length) return 0;
+  return [... arguments].reduce((prev, curr)=>prev*curr, 1)
 }
 
 
