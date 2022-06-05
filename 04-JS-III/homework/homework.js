1ec8cab6a69b930d7dfd7fef534d4429ad566130
unknown <mosqueralorenzo@hotmail+.com>
2022-06-05 18:43:56 -0300

    tarea realizada hasta la linea 146 del homework JSIII

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -133,7 +133,13 @@ function multiplicarArgumentos() {
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+  var cont = 0;
+  for (let i=0; i< arreglo.length; i++){
+       if(arreglo[i] > 18 ){
+       cont = cont++;
+    }
+ }
+   return [cont];
 }
 
 
