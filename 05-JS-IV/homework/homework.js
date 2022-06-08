e7d614195ab59dab79d0b8d2b35a79b8c87226b0
Loromhir <mosqueralorenzo@hotmail.com>
2022-06-08 19:35:12 -0300

    tareas realizadas hasta la linea 55 del homework JSIV

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -56,7 +56,11 @@ function nuevoUsuario (nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
-      var objeto =[...[objecto]]
+      var objeto ={
+        nombre: nombre,
+        email: email,
+        password: password
+      };
       return objeto;
 }
 
