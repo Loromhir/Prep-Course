19016298c77c4710bdc9b7eb73e40eba21526b66
Loromhir <mosqueralorenzo@hotmail.com>
2022-06-09 16:29:00 -0300

    tareas realizadas hasta la linea 121 del homework JSIV

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -113,6 +113,9 @@ function agregarAmigo (usuario, nuevoAmigo) {
   // Agrega "nuevoAmigo" al final de ese array
   // Devuelve el objeto "usuario"
   // Tu código:
+        usuario.amigos.push(nuevoAmigo);
+      return usuario;
+
 }
 
 function pasarUsuarioAPremium (usuarios) {
