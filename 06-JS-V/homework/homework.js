d5a2913abb9fe2aefc07e0abc8c4882d3049db5a
Loromhir <mosqueralorenzo@hotmail.com>
2022-06-14 05:27:40 -0300

    tareas realizadas hasta la linea 44 del homework JSV

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -36,6 +36,9 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
+  String.prototype.reverse=function(){
+    this.String.reverse()
+  }
 }
 
 // ---------------------------------------------------------------------------//
