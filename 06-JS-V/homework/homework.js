62d71436b8a140818f2ea3186082546044318924
Loromhir <mosqueralorenzo@hotmail.com>
2022-06-14 14:14:01 -0300

    todas las tareas realizadas del homework JSV

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -59,13 +59,10 @@ function agregarStringInvertida() {
       this.apellido = apellido,
       this.edad = edad,
       this.domicilio = domicilio,
-      detalle = function(){
-        return {nombre : this.nombre,
+      detalle = {nombre : this.nombre,
         apellido : this.apellido,
         edad : this.edad, 
         domicilio : this.domicilio}
-      }
-
     }
 }
 
@@ -73,7 +70,7 @@ function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
-  var persona = new Persona(nombre, apellido, edad, dir);
+  var persona = new Persona("Juan", "Perez", 22, "Saavedra 123");
   return persona;
 }
   
