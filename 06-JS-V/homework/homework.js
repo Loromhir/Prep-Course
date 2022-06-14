1bde0804dfa003d115992539277dd096013a850b
Loromhir <mosqueralorenzo@hotmail.com>
2022-06-14 05:09:00 -0300

    tareas realizadas hasta la linea 26 del homework JSV

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,11 +8,11 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:}
-  function Usuario(usuario, nombre, email, password){
-    this.usuario = usuario,
-    this.nombre = nombre,
-    this.email= email,
-    this.password= password
+  function Usuario(opciones){
+    this.usuario = usuario.opciones,
+    this.nombre = nombre.opciones,
+    this.email= email.opciones,
+    this.password= password.opciones
   };
   Usuario.prototype.saludar= function(){
     return 'Hola, mi nombre es ' + this.nombre;
