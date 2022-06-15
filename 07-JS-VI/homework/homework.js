63d3551be0b60589c94cf7f18167ffffa9991ba5
Loromhir <mosqueralorenzo@hotmail.com>
2022-06-15 15:21:56 -0300

    todas las tareas realizadas del homework JSVI

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -57,7 +57,8 @@ function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
-  var arreglo= array.filter(e=> e.charAt(0)= 'a');
+  var arreglo= array.filter(function(e){ 
+    e.charAt(0)= 'a'});
   return arreglo;
 }
 
