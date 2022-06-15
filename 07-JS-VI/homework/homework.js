a0c77de50e9aef7b8a8f85812379904c4260fb1b
Loromhir <mosqueralorenzo@hotmail.com>
2022-06-15 19:11:06 -0300

    todas las tareas realizadas del homework JSVI

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -45,11 +45,11 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
-  arreglo = [];
- return array.map(function (e) {
-    elemento = cb(e);
-    arreglo.push(elemento)
+  var arreglo = array.map(function (e) {
+    return e;
   })
+  array.forEach(cb);
+  return arreglo;
 }
 
 function filter(array) {
